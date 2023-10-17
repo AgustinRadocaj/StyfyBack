ab8b1f782663bdd2aa5a0198e642bb8d3bcb690a
Agustin Radocaj <jaguscaj@gmail.com>
2023-10-17 20:50:22 -0300

    port fix

--- back/index.js
+++ back/index.js
@@ -2,7 +2,7 @@ const server = require("./src/server");
 const { conn, createDefaultAdminUser } = require('./src/db.js');
 const http = require('http');
 const { createBooks } = require("./src/controllers/getBooksController");
-const PORT = 80;
+const PORT = 3000;
 
 conn.sync({ alter: true }).then( async () => {
   await createBooks();
@@ -10,7 +10,7 @@ conn.sync({ alter: true }).then( async () => {
   const httpServer = http.createServer(server);
   httpServer.timeout = 300000;
 
-  server.listen(PORT, () => {
+  server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server listening on port ${PORT}`);
 })
 }).catch(error => console.error(error));
