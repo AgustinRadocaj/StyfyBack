91e6ec084c40c5d750f03651e9d5870002c08913
Agustin Radocaj <jaguscaj@gmail.com>
2023-10-19 01:06:10 -0300

    back deploy update

--- back/src/controllers/books/getBooksController.js
+++ back/src/controllers/books/getBooksController.js
@@ -21,7 +21,6 @@ const createBooks = async () => {
   if (dbBooks.length < 1) {
     for (const response of responses) {
       response.data.forEach(async (book) => {
-        console.log("stock:", book.stock)
         await Book.create({
           id: book.id,
           title: book.title,
@@ -29,15 +28,15 @@ const createBooks = async () => {
           publisher: book.publisher,
           image: book.image ? book.image : DEFAULT_IMAGE,
           publishedDate: book.publishedDate,
-          pageCount: book.stock,
+          pageCount: book.pageCount,
           genre: book.gender,
-          cantidad: Math.round(Math.random() * (5 - 2) + 2),
           price: Math.ceil(book.price),
           // arsPrice: Math.ceil(book.price * 843),
           // copPrice: Math.ceil(book.price * 4200),
           // mxnPrice: Math.ceil(book.price * 18),
           description: book.description,
           rating: Math.round(Math.random() * (5 - 2) + 2),
+          stock: Math.round(Math.random () * (100 - 0) + 0),
         });
       });
     };
