f671a6cd17999b3539b10cc3d434eef253a83371
Agustin Radocaj <jaguscaj@gmail.com>
2023-10-18 23:08:21 -0300

    update 2

--- back/src/db.js
+++ back/src/db.js
@@ -2,18 +2,18 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, DATABASE_URL } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_URL } = process.env;
 
 const defineBooks = require ('./models/Book'); 
 
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/books`, {
-//   logging: false, 
-//   native: false, 
-// });
-const sequelize = new Sequelize(DATABASE_URL, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/books`, {
   logging: false, 
   native: false, 
 });
+// const sequelize = new Sequelize(DB_URL, {
+//   logging: false, 
+//   native: false, 
+// });
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
