5397ce2e93372c7076eaaddf21eb7e79219fb54c
lautaromateol <lautaromateol@gmail.com>
2023-12-29 18:57:08 -0300

    new deploy url

--- back/src/utils.js
+++ back/src/utils.js
@@ -2,7 +2,7 @@ const API_URL = "http://localhost:5000/books"
 const DEFAULT_IMAGE = "https://images.freeimages.com/vhq/images/previews/b18/generic-book-clip-art-98540.png";
 // const ENDPOINT = "http://localhost:3001"
 // const FRONT_URL = "http://localhost:5173"
-const ENDPOINT = 'https://styfyback-production.up.railway.app'
+const ENDPOINT = 'https://stayfy-api.up.railway.app'
 const FRONT_URL = 'https://stayfy-xi.vercel.app'
 
 const DEFAULT_PROFILE_PICTURE = "https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small/default-avatar-profile-icon-of-social-media-user-vector.jpg"
